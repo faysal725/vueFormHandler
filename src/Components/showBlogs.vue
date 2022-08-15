5215fc3cb8d74550a41e3d316ea162adbc39ae79
faysal725 <afaysal725@gmail.com>
2022-08-15 10:54:11 +0600

    second commit

--- src/Components/showBlogs.vue
+++ src/Components/showBlogs.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        asdf
+    <div id="show-blogs">
+        <h1>all blog articles</h1>
+        <div v-for="blog in blogs" class="single-blog">
+            <h2>{{blog.title}}</h2>
+            <article>{{blog.body}}</article>
+        </div>
     </div>
 </template>
 
@@ -9,15 +13,34 @@ export default {
 
     data() {
         return {
-            
+            blogs:[]
         };
     },
     methods: {
         
     },
+    created(){
+        this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
+            this.blogs = data.body.slice(0,10)
+
+        })
+    }
 };
 </script>
 
 <style scoped>
+#show-blogs{
+    max-width: 800px;
+    margin:0 auto;
+}
 
+.single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee;
+}
 </style>
+
+
+
